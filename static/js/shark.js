0fbe02f3ba6be01ce5307a4fa374031d88ecaada
Kel Amede <164945334+Kel-Amede@users.noreply.github.com>
2024-07-23 19:27:52 -0400

    shark.js

--- static/js/shark.js
+++ static/js/shark.js
@@ -1,8 +1,33 @@
-//get an array of types
-let types = ['Unprovoked','Provoked','Questionable', 'Watercraft', 'Sea Disaster']
-//function to plot a count of types
-function plotTypes(sharkData, types)
-{
+console.log("js loaded");
+
+// Initialize function to load data and set up initial state
+function init() {
+    console.log("Initializing...");
+
+    d3.json("shark_attacks.incidents.json").then((data) => {
+        console.log("Data loaded:", data);
+
+        // Populate dropdown with incident IDs
+        function addList() {
+            var select = document.getElementById("selDataset");
+            for (let i = 0; i < data.length; i++) {
+                var option = document.createElement('option');
+                option.text = option.value = data[i].id;
+                select.add(option, 0);
+            }
+        }
+        addList();
+
+        // Plot shark attack types
+        let types = ['Unprovoked', 'Provoked', 'Questionable', 'Watercraft', 'Sea Disaster'];
+        plotTypes(data, types);
+    }).catch((error) => {
+        console.error("Error loading data:", error);
+    });
+}
+
+// Function to plot types of shark attacks
+function plotTypes(sharkData, types) {
     let typeCount = {
         'Unprovoked': 0,
         'Provoked': 0,
@@ -10,15 +35,16 @@ function plotTypes(sharkData, types)
         'Watercraft': 0,
         'Sea Disaster': 0
     };
-    for (let j = 0; j < sharkData.length; j++){
-        //console.log(sharkData[j].Type)
-        if (sharkData[j].Type in typeCount){
-            typeCount[sharkData[j].Type] += 1
+
+    // Count occurrences of each type
+    for (let j = 0; j < sharkData.length; j++) {
+        if (sharkData[j].Type in typeCount) {
+            typeCount[sharkData[j].Type] += 1;
         }
     }
-    console.log(typeCount)
-    let counts = Object.values(typeCount)
-    console.log(counts)
+
+    // Extract counts and plot using Plotly
+    let counts = Object.values(typeCount);
     let trace = {
         x: types,
         y: counts,
@@ -28,10 +54,31 @@ function plotTypes(sharkData, types)
     let data = [trace];
 
     let layout = {
-        title: "Shark Attacks by Type" 
+        title: "Shark Attacks by Type"
     };
 
-    Plotly.newPlot("plot", data, layout)
+    Plotly.newPlot("plot", data, layout);  // Assuming you have a <div id="plot"></div> in your HTML
 }
 
-plotTypes(sharkData, types);
+// Function to handle dropdown selection change
+function optionChanged(newid) {
+    console.log("Dropdown selected:", newid);
+    d3.json("shark_attacks.incidents.json").then((data) => {
+        let attackdata = data.find(sharkdata => sharkdata.id == newid);
+        if (attackdata) {
+            delete attackdata["_id"];
+            console.log("Selected incident data:", attackdata);
+            let panel = d3.select("#sample-metadata");
+            panel.html("");
+            for (item in attackdata) {
+                panel.append("h5").text(`${item}: ${attackdata[item]}`);
+            }
+        } else {
+            console.error("Incident ID not found:", newid);
+        }
+    }).catch((error) => {
+        console.error("Error fetching incident data:", error);
+    });
+}
+
+init(); // Call init function to start everything
